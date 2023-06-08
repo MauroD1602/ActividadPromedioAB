80b5a99a63fe5bcd0c8e9c35b437adb5e6e7999e
MauroD1602 <maurodubini1602@gmail.com>
2023-06-08 20:28:18 -0300

    Actividad #1 Menu y promedio: Resuelta

--- PromedioProyecto/PromedioProyecto/Program.cs
+++ PromedioProyecto/PromedioProyecto/Program.cs
@@ -8,8 +8,45 @@ namespace PromedioProyecto
 {
     class Program
     {
+        #region Menu
+        static void Menu()
+        {
+            Console.WriteLine("opcion");
+            Console.WriteLine("1-Agregar Valores");
+            Console.WriteLine("2-Calcular Promedio");
+            Console.WriteLine("0-Salir");
+        }
+        #endregion
         static void Main(string[] args)
         {
+            double valor;
+
+            Promedio p = new Promedio();
+
+            int opcion;
+
+            do
+            {
+                Menu();
+
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Agregue un Valor");
+                        valor = Convert.ToDouble(Console.ReadLine());
+                        p.AgregarValores(valor);
+                        break;
+                    case 2:
+                        Console.WriteLine("Promedio: {0}", p.Prom());
+                        break;
+                    default:
+                        break;
+                }
+            }
+            while (opcion != 0);
+            Console.ReadKey();
         }
     }
 }
--- PromedioProyecto/PromedioProyecto/Promedio.cs
+++ PromedioProyecto/PromedioProyecto/Promedio.cs
@@ -8,5 +8,31 @@ namespace PromedioProyecto
 {
     internal class Promedio
     {
+        private int n;
+        private double acum;
+
+        public Promedio()
+        {
+            n = 0;
+            acum = 0;
+        }
+
+        public void AgregarValores(double valor)
+        {
+            acum += valor;
+            n++;
+        }
+
+        public double Prom()
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return acum / n;
+            }
+        }
     }
 }
